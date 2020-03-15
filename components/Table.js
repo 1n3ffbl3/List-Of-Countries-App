35e2873d51a8d54dfbde4ec3c717bda5c573c4d6
1n3ffbl3 <mizgier.m@gmail.com>
2020-03-15 17:02:08 +0100

    Add style to table

--- components/Table.js
+++ components/Table.js
@@ -5,6 +5,19 @@ const TableHead = () => (
       <th>Capital</th>
       <th>Alpha2Code</th>
     </tr>
+    <style jsx>{`
+      thead th {
+        font-size : 17px;
+        font-weight: bold;
+        color: #fcc802;
+        text-align: left;
+        background: #060d44;
+        padding: 5px;
+        text-transform: uppercase;
+        border-left: none;
+      }
+    `}
+    </style>
   </thead>
 );
 
@@ -21,6 +34,23 @@ const Table = props => (
         </tr>
       ))}
     </tbody>
+    <style jsx>{`
+      table {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: left;
+        width: 100%;
+      }
+
+      td {
+        padding: 5px;
+      }
+
+      tr:nth-child(even) {
+        background: #ebf4ef;
+      }
+      
+    `}
+    </style>
   </table>
 );
 
