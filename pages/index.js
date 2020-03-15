b8211da3ec1dc9428836de6d1634540e33cc1451
1n3ffbl3 <mizgier.m@gmail.com>
2020-03-15 20:23:53 +0100

    Add pagination to display 20 records per page in table.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,40 @@
 import fetch from 'isomorphic-unfetch';
+import { useState } from 'react';
 
 import Layout from '../components/Layout';
 import Table from '../components/Table';
 
+const initialPageNumber = 1;
 
-const Index = props => (
+const getPage = (data, pageNumber, recordsPerPage) => {
+  const startIndex = (pageNumber - 1) * recordsPerPage;
+  return data.slice(startIndex, startIndex + recordsPerPage);
+}
+
+const Index = props => {
+  const [pageNumber, setPageNumber] = useState(initialPageNumber);
+  const recordsPerPage = 20;
+  const countries = props.allCountries ? props.allCountries : [];
+  const lastPageNumber = Math.ceil((countries.length-1)/recordsPerPage);
+
+  let pagedCountries = getPage(countries, pageNumber, recordsPerPage);
+
+  const prevPageHandle = () => {
+    setPageNumber(pageNumber - 1);
+  }
+
+  const nextPageHandle = () => {
+    setPageNumber(pageNumber + 1);
+  }
+
+  return (
   <Layout>
     <h1>All Countries</h1>
-    <Table countries={props.allCountries}/>
+    <Table countries={pagedCountries} />
+    <button disabled={pageNumber === initialPageNumber} onClick={() => prevPageHandle()}>Previous page</button>
+    <button disabled={pageNumber === lastPageNumber} onClick={() => nextPageHandle()}>Next page</button>
   </Layout>
-);
+)};
 
 
 Index.getInitialProps = async function() {
